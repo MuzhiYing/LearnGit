28149693dfba16f1906b96b8ff4299d37a84b837
YMz <cyxscj@126.com>
2019-11-20 21:06:11 +0800

    First Commit

---
+++ LearnGit/LearnGit/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LearnGit
+//
+//  Created by 樱木之 on 2019/11/20.
+//  Copyright © 2019 sse.bjut.com. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
